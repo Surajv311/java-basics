7576c2bac1d33b8383188862a79389f58f64d51b
Surajv <vsuraj2405@gmail.com>
2022-04-23 12:34:27 +0530

    java-8

--- practice/BasicDS_8.java
+++ practice/BasicDS_8.java
@@ -15,5 +15,42 @@ package practice;
 
 // Java Regex /Regular Expression is an API to define a pattern for searching or manipulating strings.
 
+/*
 
+Interface:
+Like a class, an interface can have methods and variables,
+but the methods declared in interface are by default abstract (only method signature, no body).
+
+// Java program to demonstrate
+// working of interface.
+import java.io.*;
+
+interface in1 {
+
+    // public, static and final
+    final int a = 10;
+
+    // public and abstract
+    void display();
+}
+
+// A class that implements the interface.
+class testClass implements in1 {
+
+    // Implementing the capabilities of interface.
+    public void display()
+    {
+        System.out.println("Geek");
+    }
+
+    // Driver Code
+    public static void main(String[] args)
+    {
+        testClass t = new testClass();
+        t.display();
+        System.out.println(a);
+    }
+}
+
+ */
 
--- practice/OuterInner_6.java
+++ practice/OuterInner_6.java
@@ -18,3 +18,38 @@ public class OuterInner_6 {
 }
 
 // Outputs 15 (5 + 10)
+
+
+
+/*
+Anonymous class
+
+Java anonymous inner class is an inner class without a name and for which only a single object
+is created. An anonymous inner class can be useful when making an instance of an object
+with certain "extras" such as overloading methods of a class or interface,
+without having to actually subclass a class.
+
+In simple words, a class that has no name is known as an anonymous inner class in Java.
+It should be used if you have to override a method of class or interface.
+Java Anonymous inner class can be created in two ways:
+
+-Class (may be abstract or concrete).
+-Interface
+
+Example:
+
+abstract class Person{
+  abstract void eat();
+}
+class TestAnonymousInner{
+ public static void main(String args[]){
+  Person p=new Person(){
+  void eat(){System.out.println("nice fruits");}
+  };
+  p.eat();
+ }
+}
+
+ */
+
+
